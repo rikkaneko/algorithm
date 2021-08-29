b66b20baca1f14e9bb9ee04e1cb5e34127127ad2
rikkaneko <rikkaneko23@gmail.com>
2021-08-29 18:24:57 +0800

    Update heapsort to avoid unnecessary copies

--- src/sorting/heapsort.rs
+++ src/sorting/heapsort.rs
@@ -1,15 +1,37 @@
-use crate::data_structures::heap::MaxHeap;
+pub fn left_child(idx: usize) -> usize { 2 * idx + 1 }
+
+pub fn right_child(idx: usize) -> usize { 2 * idx + 2 }
+
+pub fn max_heapify<T: Ord>(arr: &mut [T], size: usize, mut idx: usize) {
+	loop {
+		let left = left_child(idx);
+		let right = right_child(idx);
+		let mut largest = idx;
+		if left < size && arr[left] > arr[largest] { largest = left; }
+		if right < size && arr[right] > arr[largest] { largest = right; }
+		if largest != idx {
+			arr.swap(idx, largest);
+			idx = largest;
+		} else { break }
+	}
+}
+
+pub fn build_maxheap<T: Ord>(arr: &mut [T]) {
+	let size = arr.len();
+	for idx in (0..((size - 1) / 2)).rev() {
+		max_heapify(arr, size, idx);
+	}
+}
 
 /// Heapsort
-pub fn sort<T: Ord + Clone>(arr: &mut [T]) {
-	let mut heap = MaxHeap::from_arr(arr.to_vec());
-	heap.build_heap();
-	for i in (1..heap.size).rev() {
-		heap.arr.swap(0, i);
-		heap.size -= 1;
-		heap.heapify(0);
+pub fn sort<T: Ord>(arr: &mut [T]) {
+	build_maxheap(arr);
+	let mut size = arr.len();
+	for i in (1..size).rev() {
+		arr.swap(0, i);
+		size -= 1;
+		max_heapify(arr, size, 0);
 	}
-	arr.clone_from_slice(&heap.arr);
 }
 
 #[cfg(test)]
@@ -19,9 +41,17 @@ mod tests {
 	
 	#[test]
 	fn test_sort() {
-		let mut arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-		shuffle(&mut arr);
-		sort(&mut arr);
-		assert_eq!(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+		let mut arrx = [
+			[10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+			[4, 9, 10, 8, 9, 5, 1, 2, 3, 7],
+			[0, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+		];
+		for arr in arrx.iter_mut() {
+			shuffle(arr);
+			sort(arr);
+		}
+		assert_eq!(arrx[0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+		assert_eq!(arrx[1], [1, 2, 3, 4, 5, 7, 8, 9, 9, 10]);
+		assert_eq!(arrx[2], [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]);
 	}
 }
